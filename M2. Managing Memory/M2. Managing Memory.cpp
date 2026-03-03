6313ae0ed488ff8a5782dce6eb86c961840393ba
garrisonglass <garrison.glass@gmail.com>
2026-03-02 19:37:02 -0500

    added function definitions and print function

--- M2. Managing Memory/M2. Managing Memory.cpp
+++ M2. Managing Memory/M2. Managing Memory.cpp
@@ -1,1 +1,18 @@
 // M2. Managing Memory.cpp : Reverse Array
+
+#include <iostream>
+
+using namespace std;
+
+int* ReverseArray(const int arr[], int size);
+
+void print(const int arr[], int size);
+
+void print(cont = st int arr[], int size) 
+{
+	for (int i = 0; i < size; i++) 
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
